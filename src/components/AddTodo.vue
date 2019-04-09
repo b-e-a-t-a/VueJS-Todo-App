49d64799d6083a5e67d6d3f5320ba420f22fb4e1
Beata Karolczak <karolczak.beata@gmail.com>
2019-04-09 16:33:51 +0200

    Handle http requests for todos with axios

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script>
-import uuid from 'uuid';
+// import uuid from 'uuid';
 export default {
   name: "AddTodo",
   data() {
@@ -24,7 +24,6 @@ export default {
     addTodo(e) {
       e.preventDefault();
       const newTodo = {
-        id: uuid.v4(),
         title: this.title,
         completed: false
       }
